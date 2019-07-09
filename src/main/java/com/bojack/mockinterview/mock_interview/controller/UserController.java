124847574da1bfebb988f8a0e1dff3119c3a8323
sun <sunhoumao@icloud.com>
2019-07-10 00:49:01 +0800

    返回值

--- src/main/java/com/bojack/mockinterview/mock_interview/controller/UserController.java
+++ src/main/java/com/bojack/mockinterview/mock_interview/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.bojack.mockinterview.mock_interview.controller;
 
+import com.bojack.mockinterview.mock_interview.entity.ResponseWrapper;
+import com.bojack.mockinterview.mock_interview.entity.ReturnCode;
 import com.bojack.mockinterview.mock_interview.entity.User;
 import com.bojack.mockinterview.mock_interview.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,49 +11,56 @@ import java.util.Date;
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "/users", method = { RequestMethod.GET, RequestMethod.POST })
+@RequestMapping(value = "/users", method = {RequestMethod.GET, RequestMethod.POST})
 public class UserController {
-
     @Autowired
     private UserService userservice;
 
     @RequestMapping("/listAll")
     @ResponseBody
-    public List<User> ListUser(){
+    public List<User> ListUser() {
         return userservice.listUser();
     }
 
     @RequestMapping("/ListUserByname")
     @ResponseBody
-    public User listUserByName(@RequestParam("userName") String userName){
-        return userservice.getUser(userName);
+    public ResponseWrapper listUserByName(@RequestParam("userName") String userName) {
+        User user = userservice.getUser(userName);
+        ResponseWrapper responseWrapper = new ResponseWrapper();
+        responseWrapper.setData(user);
+        return responseWrapper;
+
     }
 
+    public ResponseWrapper listFriends(@PathVariable("userId") String userId) {
+        return null;
+    }
 
-    @RequestMapping(value = "/delete", method = RequestMethod.GET)
-    public String delete(int id) {
-        int result = userservice.delete(id);
+    @RequestMapping(value = "/delete/{userId}", method = RequestMethod.GET)
+    public String delete(@PathVariable int userId) {
+        int result = userservice.delete(userId);
         if (result >= 1) {
             return "删除成功";
         } else {
             return "删除失败";
         }
     }
-/*
-    @RequestMapping(value = "/update", method = RequestMethod.POST)
-    public String update(User user) {
-        int result = userservice.Update(user);
-        if (result >= 1) {
-            return "修改成功";
-        } else {
-            return "修改失败";
-        }
 
-    }
-*/
+    /*
+        @RequestMapping(value = "/update", method = RequestMethod.POST)
+        public String update(User user) {
+            int result = userservice.Update(user);
+            if (result >= 1) {
+                return "修改成功";
+            } else {
+                return "修改失败";
+            }
+
+        }
+    */
     @RequestMapping(value = "/insert", method = RequestMethod.POST)
-    public User insert(@RequestParam("userName") String userName,@RequestParam("password") String password) {
-        User user=new User();
+    public User insert(@RequestParam("userName") String userName, @RequestParam("password") String password) {
+        User user = new User();
         user.setUserName(userName);
         user.setPassword(password);
         user.setAge(10);
